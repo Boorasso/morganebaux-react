749ac25f1a5e06fe21704c66d1fc158a0255c731
Thomas Bourasseau <tom.bourasseau@gmail.com>
2021-04-06 13:11:54 +0200

    configure token

--- src/prismic-configuration.js
+++ src/prismic-configuration.js
@@ -1,19 +1,19 @@
-import Prismic from 'prismic-javascript'
+import Prismic from "prismic-javascript";
 
 // -- Prismic API endpoint
-// Determines which repository to query and fetch data from
-export const apiEndpoint = 'https://morganebaux-react.cdn.prismic.io/api/v2'
+export const apiEndpoint = "https://morganebaux-react.cdn.prismic.io/api/v2";
 
 // -- Access Token if the repository is not public
 // Generate a token in your dashboard and configure it here if your repository is private
-const accessToken = ''
+const accessToken =
+  "MC5ZR3hCdXhRQUFDTUFLemxx.a1nvv71jT--_ve-_vVgtIDrvv71_ewfvv73vv73vv71H77-9YO-_ve-_vSfvv71S77-977-977-9Yzho";
 
 // -- Link resolution rules
 // Manages the url links to internal Prismic documents
 export const linkResolver = (doc) => {
-  if (doc.type === 'page') return `/page/${doc.uid}`
-  return '/'
-}
+  if (doc.type === "page") return `/page/${doc.uid}`;
+  return "/";
+};
 
 // Client method to query documents from the Prismic repo
-export const client = Prismic.client(apiEndpoint, { accessToken })
+export const client = Prismic.client(apiEndpoint, { accessToken });
