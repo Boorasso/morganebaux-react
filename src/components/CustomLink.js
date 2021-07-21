45558a9063ba823f2a00d6737806ebd6101486ff
Thomas Bourasseau <tom.bourasseau@gmail.com>
2021-07-21 14:59:47 +0200

    make CustomLink capable of rendering external link

--- src/components/CustomLink.js
+++ src/components/CustomLink.js
@@ -3,11 +3,19 @@ import { Link } from "react-router-dom";
 import { linkResolver } from "../prismic-configuration";
 
 const CustomLink = (type, element, content, children, index) => {
-  return (
-    <Link key={element.data.id} to={linkResolver(element.data)}>
-      {content}
-    </Link>
-  );
+  if (element.data.link_type === "Web") {
+    return (
+      <a key={element.data.id || index} href={element.data.url} target="_blank">
+        {content}
+      </a>
+    );
+  } else {
+    return (
+      <Link key={element.data.id || index} to={linkResolver(element.data)}>
+        {content}
+      </Link>
+    );
+  }
 };
 
 export default CustomLink;
